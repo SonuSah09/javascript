08b8dd40048f4d41977faf5c523eaca5bd0ea38f
sonu69469 <97663885+sonu69469@users.noreply.github.com>
2026-01-20 08:37:40 +0000

    added progress

--- 01basics/01_variables.js
+++ 01basics/01_variables.js
@@ -4,6 +4,15 @@ var accountPassword = "655588776"
 accountCity =  "Kolkata"
 
 
-accountId = 8487348
+// accountId = 8487348 -- not allowed -- assigning something to constant is not allowed.
+/*
+prefer not to use "var" to declare variables in javascript
+the reason -  var is  having a problem with block scopes and functional scope ({anything inside the curly braces are called scope}) 
+*/
 
-console.log(accountId);
+accountEmail = "abcjdcj@gml.com"
+accountPassword = "cbgsajdfv47"
+let accountState;
+// note -  if we just create a variable and don't assign any value then javascript will consider it as undefined - for example, accountState
+console.log( "Sonu, here's your Account id-", accountId);
+console.table ([accountId, accountEmail, accountPassword, accountCity, accountState]);
