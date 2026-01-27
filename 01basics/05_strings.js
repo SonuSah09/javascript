25f6b92f0e8986a2e4c476039b523df453b57401
SonuSah <97663885+SonuSah09@users.noreply.github.com>
2026-01-27 16:01:04 +0000

    pprogres on Strings

--- 01basics/05_strings.js
+++ 01basics/05_strings.js
@@ -1,9 +1,11 @@
 const name = "Sonu"
 const repoCount = 5
 
-// console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value"); // Sonu5 value => not recommended to use this way as it's outdated
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // Hello my name is Sonu and my repo count is 5
+console.log(`Hello, This is ${name} and i have around ${repoCount} repos on my channel`); // Hello, This is Sonu and i have around 5 repos on my channel
+
 
 const gameName = new String('hitesh-hc-com')
 
