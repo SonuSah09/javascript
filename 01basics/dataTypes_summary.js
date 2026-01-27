74fc19abc80f5ff185d582cbff8e0048282a422c
SonuSah <97663885+SonuSah09@users.noreply.github.com>
2026-01-27 15:24:45 +0000

    added memory progress

--- 01basics/dataTypes_summary.js
+++ 01basics/dataTypes_summary.js
@@ -46,6 +46,7 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
+/*
 console.log(typeof score); // number
 console.log(typeof scoreValue); // number
 console.log(typeof isLoggedIn); // boolean
@@ -61,11 +62,7 @@ console.log(typeof myFunction); // function
 
 console.log(myObj); // name: 'sonu' , age: 22
 
-
-
-
-
-
+*/
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
@@ -90,3 +87,39 @@ Return type of variables in JavaScript
        Function  =>  function
        Object  =>       object
 */
+
+// ===================================================================================================
+
+//                                       Types of Memory
+
+// 1. Stack Memory : Used for all premitive datatypes
+// 2. Heap Memory : Used for refrence or non-premitive datatypes
+
+// Example for Stack Memory
+
+let myHometown = "Begusarai" // now this will be stored in Stack memory
+
+let myCurrentCity = myHometown
+
+console.log(myCurrentCity); // Begusarai
+
+myCurrentCity = "New Delhi"
+
+console.log(myHometown); // Begusarai  
+console.log(myCurrentCity); // New Delhi
+
+// Example for Heap Memory:
+
+let userOne = {
+    Email: "sonu@yahoo.com",
+    upiId: "sonu@ybl"
+}
+let userTwo = userOne
+userTwo.Email = "changed@email.com"
+
+console.log(userOne.Email); // changed@email.com
+console.log(userTwo.Email); // changed@email.com
+
+
+
+//====================================================================================================
