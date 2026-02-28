cdee0a5dc74a609aa7a8a27e6e2c94f750db435b
SonuSah <97663885+SonuSah09@users.noreply.github.com>
2026-02-28 16:38:22 +0000

    arrow functions

---
+++ 03bascs/03_arrow.js
@@ -0,0 +1,76 @@
+/*                                   ++++++++++++++++++ THIS Keyword  ++++++++++++++++++       
+
+Notes: This context works only in Objects. In function it will not work 
+*/
+
+const user = {
+    username: "Jofra",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to website`); // NOTE: this => this keyword is used to refer current context.
+        console.log(this); // Note: this will print the current context of the object (i.e- {username: "Jofra", price: 999, welcomeMessage: [Function: welcomeMessage]}) whenever we call this method. 
+    }
+
+}
+
+// user.welcomeMessage() // Jofra, welcome to website => also will print the object 'user' with current context as inside the method, we are printing 'this' 
+
+// user.username = "Sam" // now this will change the current context, means this will change the value of username in the 'user' object
+
+// user.welcomeMessage() // Sam, welcome to website => also will print the object 'user' with current context as inside the method, we are printing 'this'
+
+// console.log(this); // {} => Note: as we are running in node environment, it will give empty object because currently there is no context inside the global. But in browsers, it will not give an empty object as there is window object present inside the global in a browser engine. 
+
+
+/*
+function chai(){
+    let username = "Chris"
+    console.log(this); // Now this will print multiple context which is present for this function in node environment
+    console.log(this.username); // if we call function chai() now then this will be undefined because This keyword is used to access context inside the object not functions.
+    
+}
+
+chai()
+ 
+
+const moreChai = function () {
+    let username = "Alex"
+    console.log(this.username);
+}
+
+moreChai() // undefined and the reason is same as we saw in 'chai' function
+*/
+
+/*                                   ++++++++++++++++++   Arrow Function   ++++++++++++++++++                                          
+
+*/
+
+const anotherChai =  () => {
+    let username = "Robb"
+    console.log(this);
+}
+
+
+anotherChai() // {} => will return the empty object
+
+/*
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
+
+*/
